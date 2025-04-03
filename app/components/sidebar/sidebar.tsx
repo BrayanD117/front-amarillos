4cd5608fad59c0315a3481bc29e091b02f154055
Brayan Patiño <brayandavid1928@gmail.com>
2025-04-03 09:39:30 -0500

    Add logout in sidebar

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoCloseSharp } from "react-icons/io5";
 
@@ -13,9 +13,27 @@ const Sidebar = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleLogout = async () => {
+    try {
+      await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/autenticacion/logout`, {
+        method: 'POST',
+        credentials: 'include',
+      });
+      router.push("/");
+    } catch (error) {
+      console.error('Error al cerrar sesión:', error);
+    }
+  };
+
   return (
-    <div className={`h-screen p-4 shadow-md transition-transform duration-300 ease-in-out ${isOpen ? 'bg-yellow-100 translate-x-0' : 'bg-yellow-100 -translate-x-full'} md:translate-x-0`}>
-      <button 
+    <div
+      className={`h-screen p-4 shadow-md transition-transform duration-300 ease-in-out ${
+        isOpen
+          ? "bg-yellow-100 translate-x-0"
+          : "bg-yellow-100 -translate-x-full"
+      } md:translate-x-0`}
+    >
+      <button
         className="mb-4 p-2 bg-yellow-200 rounded cursor-pointer"
         onClick={toggleSidebar}
       >
@@ -26,7 +44,7 @@ const Sidebar = () => {
           <li>
             <button
               className="w-full text-left bg-transparent hover:bg-yellow-200 text-gray-700 font-medium py-2 px-4 rounded cursor-pointer"
-              onClick={() => router.push('/admin/panel')}
+              onClick={() => router.push("/admin/panel")}
             >
               Inicio
             </button>
@@ -34,7 +52,7 @@ const Sidebar = () => {
           <li>
             <button
               className="w-full text-left bg-transparent hover:bg-yellow-200 text-gray-700 font-medium py-2 px-4 rounded cursor-pointer"
-              onClick={() => router.push('/admin/usuarios')}
+              onClick={() => router.push("/admin/usuarios")}
             >
               Usuarios
             </button>
@@ -42,13 +60,24 @@ const Sidebar = () => {
           <li>
             <button
               className="w-full text-left bg-transparent hover:bg-yellow-200 text-gray-700 font-medium py-2 px-4 rounded cursor-pointer"
-              onClick={() => router.push('/admin/vehiculos')}
+              onClick={() => router.push("/admin/vehiculos")}
             >
               Vehículos
             </button>
           </li>
         </ul>
       )}
+      <div className="mt-5">
+        <button
+          className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg"
+          onClick={() => {
+            handleLogout();
+            router.push("/");
+          }}
+        >
+          Cerrar Sesión
+        </button>
+      </div>
     </div>
   );
 };
